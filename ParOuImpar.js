d0fd8a5c5dec9e943a53e446df4a32c7e533c691
Oevidente <andreluiz1902@gmail.com>
2021-10-16 11:29:04 -0300

    Exercício Par ou Ímpar

---
+++ ParOuImpar.js
@@ -0,0 +1,11 @@
+  //Receber uma quantidade de valores para avaliar
+  //Função exibe se cada valor é par ou impar
+  exibirTipo(10);
+  function exibirTipo(limite){
+    for(let i = 1; i<= limite; i++){
+      if(i % 2 === 0)
+        console.log(i,'PAR');
+      else
+        console.log(i,'IMPAR');
+    }
+  }
