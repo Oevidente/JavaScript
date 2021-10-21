a694b7e826fcf2cd5d056681d577db49335b0ed5
Oevidente <andreluiz1902@gmail.com>
2021-10-21 15:43:58 -0300

    Template Literal

---
+++ TemplateLiteral.js
@@ -0,0 +1,10 @@
+//Template Literal
+const mensagem = 'Oi, isso é a minha primeira mensagem.';
+const nome = 'André';
+const outra = `Oi eu sou ${nome} e esse é um exemplo de como funciona
+um "Template Literal".
+
+Espero que esse exemplo lhe seja útil.
+
+🤗`;
+console.log(outra);
