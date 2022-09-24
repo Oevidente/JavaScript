973150b74ca31bf73595c0dba0bdd372eddac577
André Luiz Alves <andreluiz1902@gmail.com>
2022-09-24 13:21:51 -0300

    Corrige retorno das funções

--- ExercicioSemPantim/ex01.js
+++ ExercicioSemPantim/ex01.js
@@ -13,15 +13,15 @@ function ataqueRapido(cavaleiroAcordado) {
 ataqueRapido(false);
 
 function furtivo(cavaleiroAcordado, arqueiroAcordado, prisioneiroAcordado) {
-  let acordados = 0;
+  let awake = 0;
   if (cavaleiroAcordado == true) {
-    acordados++;
+    awake++;
   } else if (arqueiroAcordado == true) {
-    acordados++;
+    awake++;
   } else if (prisioneiroAcordado == true) {
-    acordados++;
+    awake++;
   }
-  if (acordados > 0) {
+  if (awake > 0) {
     console.log('Pode espionar');
     return true;
   } else {
@@ -50,17 +50,98 @@ function libertarPrisioneiro(
   prisioneiroAcordado,
   cachorroEsta,
 ) {
-  if (cachorroEsta === true) {
-    if (arqueiroAcordado == false) {
-      console.log('Pode atacar com o cachorro e libertar o prisioneiro');
-    } else {
-      if (prisioneiroAcordado == true) {
-        if (cavaleiroAcordado == false && arqueiroAcordado == false) {
-          console.log('Não pode resgatar o prisioneiro');
-        }
+  if (cachorroEsta == true && arqueiroAcordado == false) {
+    console.log('Pode atacar com o cachorro e libertar o prisioneiro');
+    return true;
+  } else {
+    if (prisioneiroAcordado == true) {
+      if (cavaleiroAcordado == false && arqueiroAcordado == false) {
+        console.log('Pode libertar o prisioneiro');
+        return true;
+      } else {
+        console.log('Não pode libertar o prisioneiro');
+        return false;
       }
+    } else {
+      console.log('Não pode libertar o prisioneiro');
+      return false;
     }
   }
 }
 
 libertarPrisioneiro(false, true, false, false);
+
+//--------------------------------------------------------------------------
+
+export function canExecuteFastAttack(knightIsAwake) {
+  switch (knightIsAwake) {
+    case (knightIsAwake = true):
+      console.log('Não pode atacar rápido');
+      return false;
+      break;
+    case (knightIsAwake = false):
+      console.log('Pode atacar rápido');
+      return true;
+      break;
+  }
+}
+//canExecuteFastAttack(false);
+
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  let awake = 0;
+  if (knightIsAwake == true) {
+    awake++;
+  } else if (archerIsAwake == true) {
+    awake++;
+  } else if (prisonerIsAwake == true) {
+    awake++;
+  }
+  if (awake > 0) {
+    console.log('Pode espionar');
+    return true;
+  } else {
+    console.log('Não pode espionar');
+    return false;
+  }
+}
+
+//canSpy(false, true, false);
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  if (archerIsAwake == false && prisonerIsAwake == true) {
+    console.log('Pode alertar o prisioneiro');
+    return true;
+  } else {
+    console.log('Não pode alertar o prisioneiro');
+    return false;
+  }
+}
+
+//canSignalPrisoner(false, true);
+
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent,
+) {
+  if (petDogIsPresent == true && archerIsAwake == false) {
+    console.log('Pode atacar com o cachorro e libertar o prisioneiro');
+    return true;
+  } else {
+    if (prisonerIsAwake == true) {
+      if (knightIsAwake == false && archerIsAwake == false) {
+        console.log('Pode libertar o prisioneiro');
+        return true;
+      } else {
+        console.log('Não pode libertar o prisioneiro');
+        return false;
+      }
+    } else {
+      console.log('Não pode libertar o prisioneiro');
+      return false;
+    }
+  }
+}
+
+//canFreePrisoner(false, true, false, false);
